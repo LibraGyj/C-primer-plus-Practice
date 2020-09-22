6177762d21ca42dceae4ea50f317aaef81301e2b
LibraGyj <70457517+LibraGyj@users.noreply.github.com>
2020-09-22 22:13:43 +0800

    Update chapter6-18.c

--- chapter6-18.c
+++ chapter6-18.c
@@ -5,7 +5,7 @@ int main (void)
 	int w, t = 5;
 	
 	w = 1;
-	while(t <= 150)
+	while(t <= DUNBAR)
 	{
 		t = 2 * (t - w);
 		printf("%d  %d\n",w, t);
@@ -14,4 +14,4 @@ int main (void)
 	printf("%d  %d",w - 1, t);
 	
 	return 0;
-}
+} //一个没什么技术含量的程序，但为了庆祝chapter6的结束，我还是把它放上来了，✿✿ヽ(°▽°)ノ✿
