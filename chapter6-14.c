aaed7f7814e20510fa09aefcf5a7362200b02dc0
LibraGyj <70457517+LibraGyj@users.noreply.github.com>
2020-09-22 20:46:16 +0800

    Add files via upload

---
+++ chapter6-14.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main(void)
+{
+	const int S = 7, Z = 7;
+	int shuru[S], zonghe[Z], sum;
+	int s, z;
+	
+    for(s = 0; s <= S; s++)
+    {
+    	printf("请输入第%d个数字:\n", s+1);
+    	scanf("%d", &shuru[s]);
+	}
+	for(z = 0,s = 0; z <= Z; z++, s++)
+	{
+		sum += shuru[s];
+		zonghe[z] = sum;
+	}
+	s = 0;
+    while(s <= S)
+    {
+    	printf("%d\t", shuru[s]);
+    	s++;
+	}
+	printf("\n");
+ 
+    z = 0;
+    while(z <= Z)
+    {
+    	printf("%d\t", zonghe[z]);
+    	z++;
+	}
+    
+    return 0;
+}
+    
+    
+    
